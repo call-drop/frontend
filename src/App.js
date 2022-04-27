a535841559d9ee0cac04af621a8c27cece93405d
Heemank Verma <heemankv@gmail.com>
2022-04-28 02:30:45 +0530

    linker added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
-import './App.css';
+import "./App.css";
 import Home from "./components/home";
-import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Ticket from "./components/ticket";
 import CallLog from "./components/callLog";
 import Plan from "./components/plan";
@@ -13,26 +12,29 @@ import CustomerList from "./components/employee/customerList";
 import Profile from "./components/profile";
 import Tower from "./components/employee/tower";
 import OpenTickets from "./components/employee/openTickets";
+import Linker from "./components/linker";
 
 function App() {
   return (
     <div className="App">
-        <Home />
-        <Router>
-            <Routes>
-                <Route path='/ticket' element={<Ticket />}/>
-                <Route path='/call_Log' element={<CallLog />}/>
-                <Route path='/plan' element={<Plan />}/>
-                <Route path='/signup' element={<SignUp />}/>
-                <Route path='/login' element={<Login />}/>
-                <Route path='/sms' element={<SMS />}/>
-                <Route path='/call' element={<Call />}/>
-                <Route path='/customerList' element={<CustomerList />}/>
-                <Route path='/profile' element={<Profile />}/>
-                <Route path='/towerCheck' element={<Tower />}/>
-                <Route path='/openTickets' element={<OpenTickets />}/>
-            </Routes>
-        </Router>
+      <Home />
+      <Router>
+        <Routes>
+          <Route path="/ticket" element={<Ticket />} />
+          <Route path="/call_Log" element={<CallLog />} />
+          <Route path="/plan" element={<Plan />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sms" element={<SMS />} />
+          <Route path="/call" element={<Call />} />
+          <Route path="/customerList" element={<CustomerList />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/towerCheck" element={<Tower />} />
+          <Route path="/linker" element={<Linker />} />
+
+          <Route path="/openTickets" element={<OpenTickets />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
