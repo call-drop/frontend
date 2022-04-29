06dea2b6b4ac5a8804cb02e141a8091c8028e462
Heemank Verma <heemankv@gmail.com>
2022-04-29 16:53:10 +0530

    home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 import CustomNav from "./navbar";
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-          <CustomNav />
+        <CustomNav />
+        <p>
+          <h6>This is</h6>
+          <h1>Call Drop.</h1>
+        </p>
+        <p>
+          Made by:
+          <br />
+          Aditiya Pratap Singh || 2020016
+          <br />
+          Anis Mishra || 2020026
+          <br />
+          Heemank Verma || 2020064
+          <br />
+          Shikhar Sharma || 2020121
+        </p>
       </div>
     );
   }
@@ -14,19 +29,19 @@ export default class Home extends React.Component {
 // ticket page
 // kyc checker/signup and login
 // customer features
-     // ticket  radio for tower or phone
-    //call log
-    //plan
-    //payment
-    //profile
-    //sms
-    //call doing
-    //data log
+// ticket  radio for tower or phone
+//call log
+//plan
+//payment
+//profile
+//sms
+//call doing
+//data log
 
 // employee features
-    // create plan
-    // payment
-    // profile
-    // incomplete kyc
-    // tower to be serviced and marked
-    // open tickets view
+// create plan
+// payment
+// profile
+// incomplete kyc
+// tower to be serviced and marked
+// open tickets view
